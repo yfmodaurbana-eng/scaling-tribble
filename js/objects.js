6ae1579bba21c61c3249f9c7649213d46fc2e133
Fran Moreno <yf.modaurbana@gmail.com>
2026-08-08 17:41:41 +0200

    Refactor crearObjeto to accept parameters and log details

--- js/objects.js
+++ js/objects.js
@@ -1,5 +1,28 @@
-console.log("A");
+```javascript
+console.log("OBJECTS PASO 3");
 
-window.crearObjeto = function() {
-    console.log("B");
+window.crearObjeto = function(tipo, icono) {
+
+    console.log("CREANDO:", tipo);
+
+    var tanque =
+        document.querySelector(".tank-3d");
+
+    console.log("TANQUE:", tanque);
+
+    var objeto =
+        document.createElement("div");
+
+    objeto.className =
+        "objeto " + tipo;
+
+    objeto.textContent =
+        icono;
+
+    tanque.appendChild(objeto);
+
+    console.log("OBJETO AÑADIDO:", objeto);
+
+    return objeto;
 };
+```
