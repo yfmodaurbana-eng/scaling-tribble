8c5f776df42d0b45815c04254004e061140a73c0
Fran Moreno <yf.modaurbana@gmail.com>
2026-08-08 17:45:55 +0200

    Add function to create and append objects to tank

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,29 @@
-console.log("PRUEBA-OBJETOS-123");
+console.log("OBJECTS ENGINE V8");
+
+window.crearObjeto = function(tipo, icono) {
+
+    var tanque = document.querySelector(".tank-3d");
+
+    if (!tanque) {
+        console.error("NO EXISTE TANK-3D");
+        return;
+    }
+
+    var objeto = document.createElement("div");
+
+    objeto.className = "objeto " + tipo;
+
+    objeto.textContent = icono;
+
+    objeto.style.position = "absolute";
+    objeto.style.left = "50%";
+    objeto.style.top = "50%";
+    objeto.style.zIndex = "300";
+    objeto.style.fontSize = "40px";
+
+    tanque.appendChild(objeto);
+
+    console.log("OBJETO CREADO", tipo);
+
+    return objeto;
+};
