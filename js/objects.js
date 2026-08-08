1a01bca846a1e06f98189c4aee537de964465a6f
Fran Moreno <yf.modaurbana@gmail.com>
2026-08-08 17:48:51 +0200

    Enhance object creation with 3D properties
    Updated object creation to include 3D coordinates and changed positioning.

--- js/objects.js
+++ js/objects.js
@@ -1,4 +1,4 @@
-console.log("OBJECTS ENGINE V8");
+console.log("OBJECTS ENGINE V8 - 3D");
 
 window.crearObjeto = function(tipo, icono) {
 
@@ -16,14 +16,44 @@ window.crearObjeto = function(tipo, icono) {
     objeto.textContent = icono;
 
     objeto.style.position = "absolute";
-    objeto.style.left = "50%";
-    objeto.style.top = "50%";
+    objeto.style.left = "0";
+    objeto.style.top = "0";
     objeto.style.zIndex = "300";
     objeto.style.fontSize = "40px";
+    objeto.style.transformStyle = "preserve-3d";
+
+    var x = Math.random() * 60 + 20;
+    var y = Math.random() * 60 + 20;
+    var z = Math.random() * 100;
+
+    objeto.dataset.x = x;
+    objeto.dataset.y = y;
+    objeto.dataset.z = z;
+
+    actualizarPosicion(objeto);
 
     tanque.appendChild(objeto);
 
-    console.log("OBJETO CREADO", tipo);
+    console.log(
+        "OBJETO 3D:",
+        x,
+        y,
+        z
+    );
 
     return objeto;
 };
+
+
+function actualizarPosicion(objeto) {
+
+    var x = Number(objeto.dataset.x);
+    var y = Number(objeto.dataset.y);
+    var z = Number(objeto.dataset.z);
+
+    objeto.style.transform =
+        "translate3d(" +
+        x + "%," +
+        y + "%," +
+        z + "px)";
+}
