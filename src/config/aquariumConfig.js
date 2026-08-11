ccdbe54cc3dfb8013cba5723257bb7f6f87ca8db
Fran Moreno <yf.modaurbana@gmail.com>
2026-08-11 02:11:50 +0200

    Add aquarium configuration settings

---
+++ src/config/aquariumConfig.js
@@ -0,0 +1,30 @@
+export const aquariumConfig = {
+  dimensions: {
+    length: 140,
+    width: 45,
+    height: 35
+  },
+
+  structure: {
+    glassThickness: 8,
+    reinforced: true,
+    braceCount: 1
+  },
+
+  water: {
+    level: 32
+  },
+
+  objects: {
+    plants: [],
+    rocks: [],
+    wood: [],
+    fish: []
+  },
+
+  view: {
+    zoom: 1,
+    rotationX: 0,
+    rotationY: 0
+  }
+};
