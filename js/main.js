08a6fb9f3cc054459d7092be503f028893de98f8
Fran Moreno <yf.modaurbana@gmail.com>
2026-08-11 12:12:55 +0200

    Add initialization log for Aquarium Studio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+// =========================================================
+// AQUARIUM STUDIO
+// MAIN JAVASCRIPT
+// =========================================================
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Aquarium Studio iniciado correctamente.");
+
+});
